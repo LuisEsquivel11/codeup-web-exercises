fd8bb5f0db539b7880017f343e0881b68c4f1bd3
Luis <LuisDEsquivel11@gmail.com>
2023-06-06 15:44:25 -0500

    Arrays lecture and exercises

--- js/arrays-lecture.js
+++ js/arrays-lecture.js
@@ -1,2 +1,23 @@
 "use strict"
-/*************************** Arrays *************/
+/*************************** Arrays ******************************************/
+ const names = ["Luis", "Mac Miller", "Method Man", "Ghostface-Killa"]
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+// console.log(names[3]);
+// console.log(names[names.length - 1])
+//
+//
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i])
+// }
+//
+// names.forEach((name, i, array) => {
+//     console.log(arr);
+//     console.log()
+// })
+
+/************for of loop ************/
+for (const name of names) {
+    console.log(name);
+}
