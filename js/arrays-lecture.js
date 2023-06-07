f6bcc6ecb6e09838e0da56e4298a068ba1c5b49d
Luis <LuisDEsquivel11@gmail.com>
2023-06-07 11:09:14 -0500

    Arrays lecture notes and array exercises

--- js/arrays-lecture.js
+++ js/arrays-lecture.js
@@ -1,6 +1,7 @@
 "use strict"
 /*************************** Arrays ******************************************/
  const names = ["Luis", "Mac Miller", "Method Man", "Ghostface-Killa"]
+console.log(names)
 // console.log(names[0]);
 // console.log(names[1]);
 // console.log(names[2]);
@@ -18,6 +19,49 @@
 // })
 
 /************for of loop ************/
-for (const name of names) {
-    console.log(name);
-}
+// for (const name of names) {
+//     console.log(name);
+// }
+
+
+/*************************************** Array Manipulation ******************************/
+//*Spread Operator to duplicate arrays
+
+names.push("SchoolBoyQ", "MF Doom"); // .push adds th back of the Array
+console.log(".push")
+console.log(names)
+
+names.unshift("Eminem")  // .unshift adds to front of the array
+console.log(".unshift")
+console.log(names)
+
+names.pop("Luis") // Moves last element in array to the 1st element
+console.log(".pop")
+console.log(names)
+
+names.shift() // removes 1st element from array
+console.log(".shift")
+console.log(names)
+
+console.log(names.indexOf("Ghostface-Killa")) // .indexOf returns index of entered element
+
+let newArr = names.slice(0, 4) // .slice returns a piece from existing array without changing array
+console.log('newArr')
+console.log(newArr)
+
+let otherNewArr = [...names]
+console.log("otherNewArr")
+console.log(otherNewArr)
+
+names.reverse()     // reverses order of the array
+console.log(".reverse")
+console.log(names)
+
+names.sort()        // sorts arr
+console.log(".sort")
+console.log(names)
+
+
+
+
+
