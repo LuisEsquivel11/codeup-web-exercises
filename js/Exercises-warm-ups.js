68daa8579d10b39623d928f24643e49b8de9684d
Luis <LuisDEsquivel11@gmail.com>
2023-06-22 21:23:08 -0500

    save point for order-pizza.html , Js function warm-up

--- js/Exercises-warm-ups.js
+++ js/Exercises-warm-ups.js
@@ -140,9 +140,24 @@ let dateConfliction = false;
 //     }
 //     return product;
 //
-function returnProduct(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        product *= nums[i];
-    }
-    return product;
+// function returnProduct(nums) {
+//     for (let i = 0; i < nums.length; i++) {
+//         product *= nums[i];
+//     }
+//     return product;
+// }
+
+//TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+var names = ['Alexis', 'Omar', 'Carlos', 'Karina']
+
+// TODO: Create a log statement that will log the number of elements in the names array.
+console.log("There are " + names.length + " names in the array");
+
+// TODO: Create log statements that will print each of the names array elements individually.
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
 }
+
+names.forEach(function (element, index, array) {
+    console.log(element);
+});
