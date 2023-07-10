0cc9a16fac9a394053986af579d689a6aaa5d1a8
Luis <LuisDEsquivel11@gmail.com>
2023-07-10 10:11:57 -0500

    js warm up

--- js/Exercises-warm-ups.js
+++ js/Exercises-warm-ups.js
@@ -413,20 +413,131 @@ names.forEach(function (element, index, array) {
     console.log(element);
 });
 
-// TODO: Show an alert message that welcomes the user based on their input.
-alert("Welcome, " + userName + "!")
-// TODO: Ask the user if they like pizza.
-//       Based on their answer show a creative alert message.
-var response = confirm("Do you like pizza, " + userName + "?")
+// // TODO: Show an alert message that welcomes the user based on their input.
+// alert("Welcome, " + userName + "!")
+// // TODO: Ask the user if they like pizza.
+// //       Based on their answer show a creative alert message.
+// var response = confirm("Do you like pizza, " + userName + "?")
 
-if (response) {
-    alert("Sweet, I do too!")
-} else {
-    alert("Really? That's weird...")
+// if (response) {
+//     alert("Sweet, I do too!")
+// } else {
+//     alert("Really? That's weird...")
+// }
+
+// 1. Create a function that prints 1 to 100.
+//
+//
+// 2. Create a function, fizzBuzz, that prints out all numbers from 1 to 100 but instead of printing
+// numbers that are evenly divisible by 3, print 'fizz' and any numbers evenly divisible
+// by 5, print 'buzz'. For any numbers that are evenly divisible by 3 AND 5, print 'fizzbuzz'.
+
+function fizzBuzz() {
+
 }
 
+/*
+fizzBuzz() // prints...
+
+1
+2
+'fizz'
+4
+'buzz'
+'fizz'
+7
+8
+'fizz'
+'buzz'
+11
+'fizz'
+13
+14
+'fizzbuzz'
+etc...
+
+*/
+
+// You are given an array of runner objects, where each object represents a
+// runner with properties name, age, and lapTimes (in seconds). Write a
+// function called getTopRunners that takes the runners array as input and
+// returns an array of top runners. A runner is considered top if their
+// average lap time is 50 seconds or faster. If there are no top runners in
+// the input array, the function should return an empty array
+
+const runners = [
+    { name: "Alice", age: 25, lapTimes: [50.5, 45.2, 48.8, 47.1] },
+    { name: "Bob", age: 28, lapTimes: [55.2, 50.7, 53.3, 52.9] },
+    { name: "Charlie", age: 24, lapTimes: [48.9, 46.3, 50.1, 49.5] },
+    { name: "David", age: 27, lapTimes: [51.4, 50.2, 49.8, 52.3] },
+    { name: "Eve", age: 26, lapTimes: [47.7, 45.9, 46.6, 48.3] },
+];
+
+// lap time is 50 seconds or faster
+
+//     function calculateLapTimeAverage(array) {
+//         let lapTimes= runners.lapTimes
+//         let sum = 0;
+//         lapTimes.forEach((element) => {
+//             sum += element;
+//         });
+//         return sum / array.length;
+//
+// }
+// console.log(calculateLapTimeAverage(runners))
+
+function getTopRunners(runnersArray) {
+    //declare topRunners array
+    let topRunners = [];
+
+    for (let runner of runnersArray) {
+        //check if top runner
+        //if true add if not do nothing
+        if(calculateLapTimeAverage(runner.lapTimes) < 50)
+            topRunners.push(runner)
+    }
+
+    function calculateLapTimeAverage(lapTimesArray) {
+        let total = 0
+        let count = 0
+
+        for (let lapTime of lapTimesArray) {
+            total += lapTime;
+            count++;
+        }
+        return total / count;
+    }
+
+    //return empty array if no top runners
+    return topRunners;
+}
+console.log(getTopRunners(runners));
+
+// HINT: You might want to create a function called calculateLapTimeAverage
 
 
+// that takes in an array of numbers and returns the average
+// Input:
+
+// Expected Output:
+// [
+//     { name: 'Alice', age: 25, lapTimes: [50.5, 45.2, 48.8, 47.1] },
+//     { name: 'Charlie', age: 24, lapTimes: [48.9, 46.3, 50.1, 49.5] },
+//     { name: 'Eve', age: 26, lapTimes: [47.7, 45.9, 46.6, 48.3] }
+// ]
+
+
+
+// BONUS: Each runner in the resulting array should be represented as an
+// object containing only the name and age properties.
+
+
+// Expected Bonus Output:
+//     [
+    // { name: "Alice", age: 18 },
+    //     { name: "Charlie", age: 19 },
+    //     { name: "Eve", age: 17 }
+    // ]
 
 
 
